0a0bb3ee362a5d1515cae399f76e31da702e5339
MarcoAZM <mamontagna100@gmail.com>
2025-05-12 14:29:22 -0300

    tentando arrumar novamente

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,5 @@
 async function fetchProfileData() {
-  const url =
-    "https://raw.githubusercontent.com/marcozmz/js-developer-portfolio/refs/heads/main/data/profile.json";
+  const url = "https://raw.githubusercontent.com/marcozmz/portfolio-js-developer/main/data/profile.json";
   const response = await fetch(url);
   const profileData = await response.json();
   return profileData;
